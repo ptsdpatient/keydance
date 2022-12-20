7198f4077b5f5b7fce0edc41c64e6775737f9f03
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2022-12-20 07:19:16 +0530

    update

--- keydance.cpp
+++ keydance.cpp
@@ -5,16 +5,28 @@
 #include <string>
 #include <map>
 #include <future>
+const int gameSpeed = 300;
+const int rows =20;
+char key1 = 'z';
+char key2 = 'x';
+char key3 = 'c';
+char key4 = 'v';
 char input='z';
-char key='z';
+char symbol = '*';
 bool gameOver = false;
 int score = 0;
 using namespace std;
-
+int intro() {
+    system("cls");
+    cout << "\n\n\t Hi there, this is a cpp game where you press keys when the symbol comes closer \n" << "\t This project was made by tansihq\n";
+    for (int i = 0; i < 120; i++) cout << '-';
+    cout <<" \n\n\t";
+    return 0;
+}
 int randomgen() {
     int randval = 0;
-    while (randval < 13) {
-        randval = rand() % 20;
+    while (randval < rows-7) {
+        randval = rand() % rows;
     }
     return randval;
 }
@@ -33,81 +45,71 @@ int musicPlayer() {
     wstring conv = wstring(musicString.begin(), musicString.end());//temporary wide string conv, later used for lpcwstr(long pointer constant wide string)
     LPCWSTR musicName = conv.c_str();//final datatype/string passed on to Playsound first parameter
     PlaySound(musicName, NULL, SND_ASYNC);
-    //std::cout << "music over";
+
     return 0;
 }
 
-/*char keyInput() {
-    //cout << input;
-    //if (key == 'q') { gameOver = true; }
-    //if (gameOver) exit(3);
-    
-    return input;
-}*/
 
 
 int gameStart() {
 
-    char z[21];
-    char x[21];
-    char c[21];
-    char v[21];
+    char z[rows+1];
+    char x[rows+1];
+    char c[rows+1];
+    char v[rows+1];
     while (!gameOver) {
 
         
         
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < rows; i++) {
 
-            if (z[i] != '*') { z[i] = ' '; }
+            if (z[i] != symbol) { z[i] = ' '; }
 
-            if (x[i] != '*') { x[i] = ' '; }
+            if (x[i] != symbol) { x[i] = ' '; }
 
-            if (c[i] != '*') { c[i] = ' '; }
+            if (c[i] != symbol) { c[i] = ' '; }
 
-            if (v[i] != '*') { v[i] = ' '; }
+            if (v[i] != symbol) { v[i] = ' '; }
 
         }
 
         if (randomgen() % 6 == 4) {
-            z[randomgen()] = '*';
-            x[randomgen()] = '*';
-            c[randomgen()] = '*';
-            v[randomgen()] = '*';
+            z[randomgen()] = symbol;
+            x[randomgen()] = symbol;
+            c[randomgen()] = symbol;
+            v[randomgen()] = symbol;
         }
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < rows; i++) {
             z[i] = z[i + 1];
             x[i] = x[i + 1];
             c[i] = c[i + 1];
             v[i] = v[i + 1];
         }
-        std::cout << "\n\n";
-
-        char keyDisplayList[4] = { 'z','x','c','v' };
+        std::cout << "\n\n\n";
+        if (_kbhit()) {
+            input = _getch(); if ((z[0] == symbol && input == key1) || (x[0] == symbol && input == key2) || (c[0] == symbol && input == key3) || (v[0] == symbol && input == key4)) { score += 100; }
+        }
+        else { input = ' '; }
+        char keyDisplayList[4] = { key1, key2, key3, key4 };
+        cout << "\t\t";
         for (char kd : keyDisplayList) std::cout << " [" << kd << "]";
         std::cout << "\t\t score: " << score << "\t input: " << input<< endl;
-        for (int i = 0; i < 10; i++) std::cout << " [" << z[i] << "] [" << x[i] << "] [" << c[i] << "] [" << v[i] << "] " << endl;
+        for (int i = 0; i < rows-10; i++) std::cout << "\t\t [" << z[i] << "] [" << x[i] << "] [" << c[i] << "] [" << v[i] << "] " << endl;
         std::cout << "\n";
-        if (_kbhit()) { input = _getch(); }
-        else input = ' ';
-        if ((z[0] == '*' && input=='z') || (x[0] == '*' && input=='x') || (c[0] == '*' && input=='c') || (v[0] == '*' && input=='v')) { score += 100; }
-       
-        Sleep(300);
+        
+        
+        
+        Sleep(gameSpeed);
 
        system("cls");
     }
     return 0;
 }
-/*int recursionFunction() {
 
-    future<char> asyncFunction = async(keyInput);
-    key = asyncFunction.get();
-    cout << key;
-    return recursionFunction();
-}*/
 int main() {
+    intro();
     musicPlayer();
-    //future<int> asyncFunction2 = async(recursionFunction);
     gameStart();
     return 0;
 }
